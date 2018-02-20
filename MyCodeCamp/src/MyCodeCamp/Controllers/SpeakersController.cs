7e97da53c69b7f10ab587ceaf6fab1b66610b13e
SL\msa <msa@tiqri.com>
2018-02-20 15:54:38 +0530

    added source

---
+++ MyCodeCamp/src/MyCodeCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+using MyCodeCamp.Filters;
+using MyCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeCamp.Controllers 
+{
+    [Route("api/camps/{moniker}/speakers")]
+    [ValidateModel]
+    public class SpeakersController : BaseController
+    {
+        private ILogger _logger;
+        private IMapper _mapper;
+        private ICampRepository _repo;
+
+        public SpeakersController( ICampRepository repo,
+                                   ILogger<SpeakersController> logger,
+                                   IMapper mapper )
+        {
+            _repo = repo;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet()]
+        public  IActionResult Get(string moniker)
+        {
+            var speakers = _repo.GetSpeakersByMoniker(moniker);
+
+            return Ok(_mapper.Map<IEnumerable<SpeakerModel>> (speakers));
+        }
+        [HttpGet("{id}",Name = "SpeakerGet")]
+        public IActionResult Get(string moniker,int id)
+        {
+            var speaker = _repo.GetSpeaker(id);
+            if(speaker == null)
+            {
+                return NotFound();
+            }
+            if(speaker.Camp.Moniker != moniker)
+            {
+                return BadRequest("Speaker not in specified camp");
+            }
+            return Ok(_mapper.Map<SpeakerModel>(speaker));
+        }
+        [HttpPost]
+        public async  Task<IActionResult> Post(string moniker,[FromBody]SpeakerModel model)
+        {
+            try
+            {
+                //if (!ModelState.IsValid)
+                //{
+                //    return BadRequest(ModelState);
+                //}
+                var camp = _repo.GetCampByMoniker(moniker);
+                if(camp == null)
+                {
+                    return BadRequest("could not find camp");
+                }
+
+                var speaker = _mapper.Map<Speaker>(model);
+
+                _repo.Add(speaker);
+                if(await _repo.SaveAllAsync())
+                {
+                    var newUri = Url.Link("SpeakerGet",new { moniker = camp.Moniker, id= speaker.Id });
+                    return Created(newUri,_mapper.Map<SpeakerModel>(speaker));
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while adding speaker : {ex}");
+            }
+            return BadRequest("Could not add a new Speaker");
+        }
+        [HttpPut("{id}")]
+        public async  Task<IActionResult> Put(string moniker,int id,[FromBody]CampModel model)
+        {
+            try
+            {
+                var speaker = _repo.GetSpeaker(id);
+                if(speaker == null)
+                {
+                    return NotFound();
+                }
+                if(speaker.Camp.Moniker != moniker)
+                {
+                    return BadRequest($"Speaker and Camp not matched");
+                }
+                _mapper.Map(model, speaker);
+                if(await _repo.SaveAllAsync())
+                {
+                    return Ok(_mapper.Map<CampModel>(speaker));
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Exception thrown while updating Speaker: {ex}");
+            }
+            return BadRequest("Couldn't update the Speaker");
+        }
+    }
+}
---
+++ MyCodeCamp/src/MyCodeCamp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+using Newtonsoft.Json;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+
+namespace MyCodeCamp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            _config = builder.Build();
+        }
+
+        IConfigurationRoot _config;
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+           // services.AddApplicationInsightsTelemetry(_config);
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+            services.AddTransient<CampDbInitializer>();
+            services.AddSingleton(_config);
+            services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+            services.AddAutoMapper();
+            services.AddMvc().AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app,
+                                IHostingEnvironment env,
+                                ILoggerFactory loggerFactory,
+                                CampDbInitializer seedr)
+        {
+            loggerFactory.AddConsole(_config.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+           //app.UseApplicationInsightsRequestTelemetry();
+
+           // app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseMvc();
+            seedr.Seed().Wait();
+        }
+    }
+}
